7b5a5272cd447e8514f0dce34f65ec6dfa4e6e5e
johnnyrpy <johnny@reepay.com>
2024-03-18 16:59:37 +0100

    (BP-803): add Xcode project

---
+++ ReepayCheckoutExample/ReepayCheckoutExample/ReepayCheckoutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReepayCheckoutExampleApp.swift
+//  ReepayCheckoutExample
+//
+//  Created by Johnny Ly on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReepayCheckoutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
